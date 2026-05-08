02174df75abbdac6efb0c814e3096b4661b643ac
atharvajavanjal03 <atharvajavanjal589@gmail.com>
2026-05-08 13:38:56 +0530

    fix user import

--- core/models.py
+++ core/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 # Create your models here.
-
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+
 
 class File(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    file = models.FileField()
+    file = CloudinaryField('file')
     uploaded_at = models.DateTimeField(auto_now_add=True)
